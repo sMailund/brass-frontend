11bdaa8376b8b8e8242e2c5cbe6256a368ae98ef
sMailund <simen.m.s@hotmail.com>
2020-04-11 17:18:53 +0200

    more work on displaying questions

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -7,9 +7,16 @@
           oppgaveside beta
         </h1>
 
-        <p class="subheading font-weight-regular">
-          {{ tasks[0].question }}
+        <p>
+          {{ currentQuestion + 1 }} / {{ correctSolves + 1}}
         </p>
+
+        <p><span v-on:click="previous">prev</span> <span v-on:click="next">next</span></p>
+
+        <p v-if="tasks[currentQuestion]" class="subheading font-weight-regular">
+          {{ tasks[currentQuestion].question }}
+        </p>
+
       </v-col>
 
     </v-row>
@@ -23,12 +30,30 @@
     name: 'HelloWorld',
 
     data: () => ({
-      tasks: []
+      tasks: [],
+      correctSolves: 0,
+      currentQuestion: 0
     }),
+
+    methods: {
+      previous: function() {
+        if (this.currentQuestion > 0) {
+          this.currentQuestion--;
+        }
+      },
+      next: function() {
+        if (this.currentQuestion < this.correctSolves) {
+          this.currentQuestion++;
+        }
+      },
+    },
 
     mounted() {
       axios.get('https://backend-dot-brass-273814.ew.r.appspot.com/task')
-              .then(response => this.tasks = response.data);
+              .then(response => {
+                this.tasks = response.data
+                this.correctSolves = response.data.reduce((solves, current) => solves + current.solved ? 1 : 0, 0);
+              });
     }
   }
 </script>
